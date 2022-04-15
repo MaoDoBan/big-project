b6a082ce2ca6f8509d4fee3e8894b88d909ba1a1
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-04-15 18:29:27 -0300

    App.vue: importando Modal.vue, registrando como componente e usando no template; movi o estilo global de body pro global.css; restringi o estilo do h1 para apenas os internos ao #app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <h1>{{ title }}</h1><br>
-  <input type="text" ref="bah"><br>
-  <button @click="clicaAqui">Clica aqui</button>
+  <Modal/>
 </template>
 
 <script>
+import Modal from "./components/Modal.vue";
+
 export default {
   name: 'App',
+  components: { Modal },
   data(){
     return {
       title: "Big Project"
     };
-  },
-  methods: {
-    clicaAqui(){
-      this.$refs.bah.focus();
-    }
   }
 }
 </script>
 
 <style>
-body{
-  background-color: #999;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -32,7 +26,7 @@ body{
   color: #2c3e50;
   margin-top: 60px;
 }
-h1{
+#app h1{
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
