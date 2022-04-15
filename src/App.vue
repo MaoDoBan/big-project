66e635f164e21c08ccc67352248893162805495e
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-04-15 13:26:22 -0300

    App.vue: substitui o boilerplate por: um h1 com o nome do projeto, um input de texto, um botão que ai ser clicado da foco no input, arrumei o css do h1 e a cor de fundo do body

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1><br>
+  <input type="text" ref="bah"><br>
+  <button @click="clicaAqui">Clica aqui</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      title: "Big Project"
+    };
+  },
+  methods: {
+    clicaAqui(){
+      this.$refs.bah.focus();
+    }
   }
 }
 </script>
 
 <style>
+body{
+  background-color: #999;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +31,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
